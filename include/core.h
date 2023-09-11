2a43fb60e46e3c9ca494b1af71640279cfcb7e42
schloing <user.datagram.protocol@proton.me>
2023-09-11 17:24:04 +1000

    file structure change, other changes that i forgot to commit

---
+++ include/core.h
@@ -0,0 +1,43 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+typedef enum {
+    COMMA            , PERIOD        , SEMICOLON     , COLON           , QUESTION_MARK   , 
+    UNDERSCORE       , PERCENTAGE    , OPEN_CURLY    , CLOSE_CURLY     , LEFT_PARENTH    ,
+    RIGHT_PARENTH    , LEFT_SQUARE   , RIGHT_SQUARE  , AMPERSAND       , ADDITION        ,
+    DIVISION         , GREATER_THAN  , LESS_THAN     , HASHTAG         , SUBTRACTION     ,
+    EXCLAMATION      , VERTICAL_BAR  , FORWARD_SLASH , BACKWARD_SLASH  ,
+
+    NUMERICAL_LITERAL, EQUALS        , LITERAL       , APOSTROPHE      , QUOTATION       ,
+    INCREMENT        , DECREMENT     , ADD_ASSIGN    , SUBTRACT_ASSIGN , ASTERISK        ,
+
+    KEYWORDS         , // KEYWORDS + (n + 1) gives keyword
+    AUTO             , BREAK          , CASE          ,
+    CHAR             , CONST          , CONTINUE      , DEFAULT         , DO              ,
+    DOUBLE           , ELSE           , ENUM          , EXTERN          , FLOAT           ,
+    FOR              , GOTO           , IF            , INLINE          , INT             ,
+    LONG             , REGISTER       , RESTRICT      , RETURN          , SHORT           ,
+    SIGNED           , SIZEOF         , STATIC        , STRUCT          , SWITCH          ,
+    TYPEDEF          , UNION          , UNSIGNED      , VOID            , VOLATILE        ,
+    WHILE            , _BOOL          , _COMPLEX      , _IMAGINARY      ,
+
+    END
+} TYPE;
+
+typedef struct {
+    char* value;
+    TYPE  type;
+} Token;
+
+extern FILE*   buffer;
+extern char    active;
+extern uint8_t G[];
+
+static inline TYPE     keyword     (const char* string);
+static inline uint16_t hash_f      (const char* key, const char* T);
+static inline uint16_t perfect_hash(const char* key);
+
+#endif
