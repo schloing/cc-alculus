7032a3e7d84d3f98885b829cfa3a785a64fbfc22
schloing <user.datagram.protocol@proton.me>
2023-09-18 14:02:22 +1000

    implement alternate forms of expressions

--- include/parser.h
+++ include/parser.h
@@ -2,14 +2,19 @@
 #define PARSER_H
 
 #include "tokens.h"
+#include <cmath>
 #include <stdint.h>
 
+typedef char* IDENTIFIER;
+
 typedef enum {
-    VARIABLE_DECLARATION,
-    FUNCTION_DECLARATION,
-    BLOCK_STATEMENT,
-    IF_STATEMENT,
-    RETURN_STATEMENT,
+    AST_VARIABLE_DECLARATION,
+    AST_FUNCTION_DECLARATION,
+    AST_BLOCK_STATEMENT,
+    AST_IF_STATEMENT,
+    AST_RETURN_STATEMENT,
+    AST_UPDATE_EXPRESSION,
+    AST_BINARY_EXPRESSION,
 } AST_TYPE;
 
 typedef enum {
@@ -17,6 +22,10 @@ typedef enum {
     INT8, INT16, INT32, INT64,
     CHAR, STRING,
 } LITERAL_FLAG;
+
+typedef enum {
+    INCREMENT, DECREMENT
+} OPERATOR;
 
 typedef struct {
     LITERAL_FLAG active;
@@ -33,7 +42,19 @@ typedef struct {
     };
 } LITERAL;
 
-typedef char* IDENTIFIER;
+// *THIS IS NOT GOOD, BUT IT IS A PROOF-OF-CONCEPT IMPLEMENTATION. WILL BE REPLACED*
+
+// this will work:
+// x == 1
+// but this wont
+// 1 == x (or anything else)
+
+typedef struct {
+    IDENTIFIER left;
+    OPERATOR   _operator;
+    LITERAL    right;
+} BINARY_EXPRESSION;
+
 typedef struct AST_NODE AST_NODE;
 
 struct AST_NODE {
@@ -41,13 +62,23 @@ struct AST_NODE {
 
     union value {
         struct FUNCTION_DECLARATION {
-            IDENTIFIER* identifer;
-            IDENTIFIER* params;
+            IDENTIFIER identifer;
+            IDENTIFIER params;
         };
+
         struct VARIABLE_DECLARATION {
+            IDENTIFIER identifier;
+            LITERAL    init;
         };
+
+        struct UPDATE_EXPRESSION {
+            OPERATOR   _operator;
+            bool       postfix; // ++x or x++
+            IDENTIFIER argument;
+        };
+
         struct IF_STATEMENT {
-            AST_NODE* test;
+            BINARY_EXPRESSION* test;
             AST_NODE* consequent;
             AST_NODE* alternative;
         };
