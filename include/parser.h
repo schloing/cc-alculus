508fb1de5c8cbbaa4a79e943dcad466a46d585e8
schloing <user.datagram.protocol@proton.me>
2023-09-18 13:50:01 +1000

    im not joking... modify AST_NODE structure AGAIN

--- include/parser.h
+++ include/parser.h
@@ -4,72 +4,54 @@
 #include "tokens.h"
 #include <stdint.h>
 
-// statement type
 typedef enum {
-    FUNCTION_DECLARATION , VARIABLE_DECLARATION , ASSIGNMENT,
-    IDENTIFIER           , LITERAL              , BINARY_EXPRESSION,
-} SMT_TYPE; 
+    VARIABLE_DECLARATION,
+    FUNCTION_DECLARATION,
+    BLOCK_STATEMENT,
+    IF_STATEMENT,
+    RETURN_STATEMENT,
+} AST_TYPE;
 
 typedef enum {
-    I32, I64,
-    F32, F64,
-    CHR, STR,
-    BIN_EXPR,
-} VALU_FLAG;
+    DOUBLE, FLOAT,
+    INT8, INT16, INT32, INT64,
+    CHAR, STRING,
+} LITERAL_FLAG;
 
-typedef enum {
-   OPER_ASSIGNMENT,
-   OPER_MULTIPLICATION,
-   OPER_DIVISION,
-   OPER_REMAINDER,
-   OPER_ADDITION,
-   OPER_SUBTRACTION,
-   OPER_BITWISE_L,
-   OPER_BITWISE_R,
-   OPER_GREATER_THAN,
-   OPER_LESS_THAN,
-   OPER_GREAT_THAN_EQUAL,
-   OPER_LESS_THAN_EQUAL,
-   OPER_EQUALITY,
-   OPER_INEQUALITY,
-   OPER_BITWISE_A,
-   OPER_BITWISE_XOR,
-   OPER_BITWISE_IOR,
-   OPER_LOGICAL_AND,
-   OPER_LOGICAL_OR,
-   OPER_ARRAY_INDEX,
-   OPER_COMMA_OPERATOR
-} BIN_OPER;
+typedef struct {
+    LITERAL_FLAG active;
 
+    union {
+        double   DOUBLE;
+        float    FLOAT;
+        uint8_t  INT8;
+        uint16_t INT16;
+        uint32_t INT32;
+        uint64_t INT64;
+        char     CHAR;
+        char*    STRING;
+    };
+} LITERAL;
+
+typedef char* IDENTIFIER;
 typedef struct AST_NODE AST_NODE;
 
 struct AST_NODE {
-    AST_NODE* left;
-    AST_NODE* right;
-    
-    VALU_FLAG flag;
+    AST_TYPE type;
 
     union value {
-        uint32_t I32;
-        uint64_t I64;
-        float    F32;
-        double   F64;
-
-        uint8_t  CHR;
-        uint8_t* STR;
-
-        struct BIN_EXPR {
-            AST_NODE* left;
-            BIN_OPER  operation;
-            AST_NODE* right;
+        struct FUNCTION_DECLARATION {
+            IDENTIFIER* identifer;
+            IDENTIFIER* params;
+        };
+        struct VARIABLE_DECLARATION {
+        };
+        struct IF_STATEMENT {
+            AST_NODE* test;
+            AST_NODE* consequent;
+            AST_NODE* alternative;
         };
     };
 };
-
-inline void parseProgram();
-
-inline AST_NODE* parseExpression();
-inline AST_NODE* parseTerm      ();
-inline AST_NODE* parseFactor    ();
 
 #endif
