7d7693480597bccd5d904362fcfe2543442877f5
schloing <user.datagram.protocol@proton.me>
2023-11-04 14:28:21 +1100

    INIT_CC() macro for easier and cleaner wasm-cc initialisation

--- src/main.c
+++ src/main.c
@@ -10,30 +10,6 @@
 #include "../include/parser.h"
 #include "../include/stdout.h"
 
-Token*    token_sequence = NULL;
-size_t    sequence_size  = 10;
-size_t    sequence_pos   = 0;
-
-AST_NODE* AST            = NULL;
-size_t    AST_size       = 10;
-size_t    AST_position   = 0;
-
-FILE*     buffer         = NULL;
-char      active         = 0;
-uint8_t   row = 1, col   = 0;
-
-KVP keywords[] =
-{
-    { "int"    , TOK_INT    },
-    { "void"   , TOK_VOID   },
-    { "char"   , TOK_CHAR   },
-    { "if"     , TOK_IF     },
-    { "else"   , TOK_ELSE   },
-    { "return" , TOK_RETURN },
-};
-
-uint8_t keywords_size = sizeof(keywords) / sizeof(keywords[0]);
-
 void freeTokens() {
     for (int i = 0; i < sequence_pos; i++)
         free(token_sequence[i].value);
@@ -50,6 +26,31 @@ void freeAST(AST_NODE* node) {
     free(node->children);
     free(node);
 }
+
+#define INIT_CC()                                       \
+    Token*    token_sequence = NULL;                    \
+    AST_NODE* AST            = NULL;                    \
+\
+    size_t    sequence_size  = 10, sequence_pos = 0;    \
+    size_t    AST_size       = 10, AST_position = 0;    \
+\
+    FILE*     buffer         = NULL;                    \
+    char      active         = 0;                       \
+    uint8_t   row = 1, col   = 0;                       \
+\
+    KVP keywords[] =                                    \
+    {                                                   \
+        { "int"    , TOK_INT    },                      \
+        { "void"   , TOK_VOID   },                      \
+        { "char"   , TOK_CHAR   },                      \
+        { "if"     , TOK_IF     },                      \
+        { "else"   , TOK_ELSE   },                      \
+        { "return" , TOK_RETURN },                      \
+    };                                                  \
+\
+    uint8_t keywords_size = sizeof(keywords) / sizeof(keywords[0]);
+
+INIT_CC();
 
 int main() {
     openBuffer("test.c");
